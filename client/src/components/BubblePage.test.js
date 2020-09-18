06b4911aa0d221f3b11dc2ab17c758de569774f8
joel vega <joelvega97@yahoo.com>
2020-09-18 17:26:59 -0400

    Made the test.

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,8 +1,49 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import axios from "axios";
+import { render, screen, waitFor } from "@testing-library/react";
 import BubblePage from "./BubblePage";
 
-test("Fetches data and renders the bubbles", () => {
+const mockFetch = jest.mock(axios.get("http://localhost:5000/api/colors"));
+
+const mockColors = [
+  {
+    data: [
+      {
+        color: "aliceblue",
+        code: {
+          hex: "#f0f8ff",
+        },
+        id: 1,
+      },
+      {
+        color: "limegreen",
+        code: {
+          hex: "#99ddbc",
+        },
+        id: 2,
+      },
+      {
+        color: "aqua",
+        code: {
+          hex: "#00ffff",
+        },
+        id: 3,
+      },
+      {
+        color: "aquamarine",
+        code: {
+          hex: "#7fffd4",
+        },
+        id: 4,
+      },
+    ],
+  },
+];
+
+test("Fetches data and renders the bubbles", async () => {
   // Finish this test
-  render(<BubblePage />);
+  mockFetch.mockResolvedValueOnce(mockColors);
+  render(<BubblePage bubbles={[]} />);
+  await waitFor(() => screen.getAllByTestId(/circle/i));
+  expect(screen.getAllByTestId(/circle/i)).toHaveLength(4);
 });
